732834d031380e46a4d067f190d9c0a30b9fc5d3
Alejandro Garcia <69361998+alegarciadelrio@users.noreply.github.com>
2024-08-01 16:14:45 -0300

    Add files via upload

---
+++ RemovePerseverationHold.ps1
@@ -0,0 +1,10 @@
+﻿$SiteURL = "https://birchandwaite.sharepoint.com/sites/thebite/"
+$ListName = "Preservation Hold Library"
+
+#Connect to PnP Online
+Connect-PnPOnline -Url $SiteURL -Interactive
+ 
+#Delete all files from the library
+Get-PnPList -Identity $ListName | Get-PnPListItem -PageSize 100 -ScriptBlock {
+    Param($items) Invoke-PnPQuery } | ForEach-Object { $_.Recycle() | Out-Null
+}
